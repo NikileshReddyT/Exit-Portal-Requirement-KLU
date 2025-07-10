9f68ca0ff7efc8fc688d69b29907c00d51a89f5d
Nikilesh Reddy <nikileshreddyt@gmail.com>
2025-07-10 14:59:06 +0530

    updated the mail service from gmail to outlook

--- exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Service/EmailService.java
+++ exit_portal_backend/src/main/java/com/jfsd/exit_portal_backend/Service/EmailService.java
@@ -1,18 +1,22 @@
 package com.jfsd.exit_portal_backend.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public void sendPasswordResetEmail(String to, String token) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("KL University - Password Reset Request");
         message.setText("Dear Student,\n\n" +
